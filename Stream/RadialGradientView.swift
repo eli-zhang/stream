34ff664535d680162fa0d6f9176bafec01ce8d67
Eli Zhang <zeliboid8@gmail.com>
2019-05-16 00:11:49 -0700

    shadows, colors, etc.

---
+++ Stream/RadialGradientView.swift
@@ -0,0 +1,61 @@
+//
+//  RadialGradientView.swift
+//  Stream
+//
+//  Created by Eli Zhang on 5/15/19.
+//  Copyright © 2019 Eli Zhang. All rights reserved.
+//
+
+import UIKit
+
+class RadialGradientView: UIView {
+    
+    let radius: CGFloat = 500
+    let colors: [CGColor] = [UIColor(red: 22/255, green: 24/255, blue: 48/255, alpha: 1.0).cgColor, UIColor.black.cgColor]
+    var gradientLayer: RadialGradientLayer!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        gradientLayer = RadialGradientLayer(radius: radius, colors: colors)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        if gradientLayer.superlayer == nil {
+            layer.insertSublayer(gradientLayer, at: 0)
+        }
+        gradientLayer.frame = bounds
+    }
+}
+
+class RadialGradientLayer: CALayer {
+    
+    var center: CGPoint {
+        return CGPoint(x: bounds.width / 2, y: bounds.height / 2)
+    }
+    var radius: CGFloat!
+    var colors: [CGColor]!
+    
+    init(radius: CGFloat, colors:[CGColor]){
+        self.radius = radius
+        self.colors = colors
+        super.init()
+        needsDisplayOnBoundsChange = true
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init()
+    }
+    
+    override func draw(in context: CGContext) {
+        context.saveGState()
+        let colorSpace = CGColorSpaceCreateDeviceRGB()
+        let locations: [CGFloat] = [0.0, 1.0]
+        guard let gradient = CGGradient(colorsSpace: colorSpace, colors: colors as CFArray, locations: locations) else { return }
+        context.drawRadialGradient(gradient, startCenter: center, startRadius: 0.0, endCenter: center, endRadius: radius, options: CGGradientDrawingOptions(rawValue: 0))
+    }
+}
--- Stream/ViewController.swift
+++ Stream/ViewController.swift
@@ -11,21 +11,28 @@ import SnapKit
 
 class ViewController: UIViewController {
 
+    var radialGradient: RadialGradientView!
     var button: ParentButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
         
+        radialGradient = RadialGradientView()
+        view.addSubview(radialGradient)
+        
         var buttons: [UIButton] = []
         for _ in 0..<6 {
             let newButton = UIButton()
-            newButton.backgroundColor = .purple
+            newButton.backgroundColor = UIColor(red: 0.92, green: 0.3, blue: 0.5, alpha: 1)
             newButton.setTitle("+", for: .normal)
             newButton.titleLabel?.font = UIFont.systemFont(ofSize: 20, weight: .black)
             newButton.titleLabel?.textAlignment = .center
             newButton.setTitleColor(.white, for: .normal)
             newButton.layer.cornerRadius = 40
+            newButton.layer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.25).cgColor
+            newButton.layer.shadowOffset = CGSize(width: 5, height: 7)
+            newButton.layer.shadowOpacity = 0.8
             newButton.layer.masksToBounds = false
             newButton.addTarget(self, action: #selector(printBoi), for: .touchUpInside)
             buttons.append(newButton)
@@ -33,12 +40,20 @@ class ViewController: UIViewController {
         
         button = ParentButton(frame: .zero, buttons: buttons, childRadius: 80)
         button.layer.cornerRadius = 40
-        button.backgroundColor = .blue
+        button.backgroundColor = UIColor(red: 0.6, green: 0.6, blue: 0.62, alpha: 1)
+        button.setImage(UIImage(named: "Custom"), for: .normal)
+        button.layer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.25).cgColor
+        button.layer.shadowOffset = CGSize(width: 5, height: 7)
+        button.layer.shadowOpacity = 0.8
+        button.layer.masksToBounds = false
         view.addSubview(button)
         setupConstraints()
     }
     
     func setupConstraints() {
+        radialGradient.snp.makeConstraints { make in
+            make.edges.equalTo(view)
+        }
         button.snp.makeConstraints { make in
             make.center.equalTo(view)
             make.width.height.equalTo(80)
